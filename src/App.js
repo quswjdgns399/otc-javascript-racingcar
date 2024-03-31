b3a4f3c20c7a9a20c82722ade779911d5288a78f
quswjdgns399 <quswjdgns3996@gmail.com>
2024-03-31 18:11:40 +0900

    feat: 에러핸들링 개선 및 작동 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import CarManager from "./CarManager.js";
+import GameManager from "./GameManager.js";
+import SyncAsyncManager from "./SyncAsyncManager.js";
+import StartGame from "./StartGame.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.carManager = new CarManager();
+    this.gameManager = new GameManager();
+    this.syncAsyncManager = new SyncAsyncManager(this.gameManager);
+    this.startGame = new StartGame(
+      this.carManager,
+      this.gameManager,
+      this.syncAsyncManager
+    );
+  }
+
+  async play() {
+    try {
+      await this.startGame.play();
+    } catch (error) {
+      Console.print(error.message);
+      if (process.env.NODE_ENV === "test") {
+        throw error;
+      }
+    }
+  }
 }
 
 export default App;
